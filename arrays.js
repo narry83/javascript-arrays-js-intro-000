7bae727f7e53e46fe4c5c798b903264f4ea7171a
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-03-07 20:01:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,14 @@
 var chocolateBars=["snickers", 'hundred grand', 'kitkat','skittles']
 
+var newArray = [];
+
 chocolateBars
+
+function addElementToBeginningOfArray(newArray, string) {
+    newArray.unshift(string)
+
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  var evenNumbers = new Array();
+}
