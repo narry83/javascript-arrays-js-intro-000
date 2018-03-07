ad6017628b28bf09673a0b466bab15fd13bd953f
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-03-07 20:10:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,11 +4,12 @@ var chocolateBars=["snickers", 'hundred grand', 'kitkat','skittles']
 
 function destructivelyAddElementToBeginningOfArray(newArray, string) {
     newArray.unshift(string)
+    return newArray
 }
 
 function addElementToBeginningOfArray(newArray, string) {
-  arrayNew=newArray
-  arrayNew.unshift(string)
+    arrayNew=newArray
+    arrayNew.unshift(string)
     return arrayNew
 
 }
